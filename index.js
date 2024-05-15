b9137b85e91f19ddb00dcc2c3babb27fb71fd330
Daniel Trejo <dwtrejo14@comcast.net>
2024-05-15 09:31:56 -0400

    Complete Lab

---
+++ index.js
@@ -0,0 +1,20 @@
+function receivesAFunction(spy) {
+    spy();
+}
+
+function returnsANamedFunction() {
+    
+    function namedFunction() {
+      
+    }
+  
+    
+    return namedFunction;
+  }
+
+  function returnsAnAnonymousFunction() {
+    
+    return function() {
+      
+    };
+  }
